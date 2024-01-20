3341d10aed1044f893902bfd20e821bd406c452c
fedeee1 <federico.martinez@vasscompany.com>
2024-01-20 17:03:44 +0100

    First component added

--- app/src/main/java/com/example/profileapp/MainActivity.kt
+++ app/src/main/java/com/example/profileapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.profileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
